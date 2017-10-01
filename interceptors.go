7121f2062597691a9645fa5c48f40bf9235b3d0a
izumin5210 <masayuki@izumin.info>
2017-10-01 13:22:11 +0900

    Add empty implementations

--- interceptors.go
+++ interceptors.go
@@ -1,1 +1,21 @@
 package grpcerrors
+
+import (
+	"context"
+
+	"google.golang.org/grpc"
+)
+
+// UnaryServerInterceptor returns a new unary server interceptor to handle errors
+func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		return handler(ctx, req)
+	}
+}
+
+// StreamServerInterceptor returns a new streaming server interceptor to handle errors
+func StreamServerInterceptor() grpc.StreamServerInterceptor {
+	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		return handler(srv, stream)
+	}
+}
