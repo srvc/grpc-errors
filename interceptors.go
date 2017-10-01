fd92e81e09aad93ef0030f14948615373cd1e082
izumin5210 <masayuki@izumin.info>
2017-10-01 18:37:11 +0900

    Use golang.org/x/net/context instead of std context for under go1.8

--- interceptors.go
+++ interceptors.go
@@ -1,8 +1,7 @@
 package grpcerrors
 
 import (
-	"context"
-
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
--- interceptors_test.go
+++ interceptors_test.go
@@ -1,11 +1,11 @@
 package grpcerrors
 
 import (
-	"context"
 	"errors"
 	"testing"
 
 	"github.com/creasty/apperrors"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
